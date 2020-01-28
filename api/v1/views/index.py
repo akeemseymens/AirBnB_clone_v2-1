a8b3a2e729ce153a1661203a13bd8818b37b7408
Ben Cook <808@holbertonschool.com>
2020-01-28 15:04:28 -0800

    add stats route to api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,18 @@
 from flask import jsonify
 from . import app_views
+from models import storage
+
 
 @app_views.route('/status')
-def index():
+def status():
     d = {'status': 'OK'}
+    return jsonify(d)
+
+
+@app_views.route('/stats')
+def stats():
+    d = {}
+    classes = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
+    for clss in classes:
+        d[clss] = storage.count(clss)
     return jsonify(d)
