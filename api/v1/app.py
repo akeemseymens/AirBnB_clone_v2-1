a5efee34afbd592582da54ac6fd140ddf2233518
Ben Cook <808@holbertonschool.com>
2020-01-30 13:35:38 -0800

    dont auto set to default mode

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,4 +25,4 @@ def page_not_found(e):
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
